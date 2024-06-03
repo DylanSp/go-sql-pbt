55626e0021f7832303b2d04156c0e183930e37fe
Dylan Sprague <ithilanor@gmail.com>
2024-06-03 00:31:57 +0000

    Adjust structure of unit tests

--- Justfile
+++ Justfile
@@ -1,7 +1,7 @@
 go_package_name := "github.com/DylanSp/go-sql-pbt"
 
 # Using "@just" instead of "just" avoids printing out "just --list"
-
+[doc]
 default:
     @just --list
 
@@ -15,5 +15,7 @@ docker-clean:
     docker compose rm -f db migrate
     docker volume rm "$(docker volume ls -q)"
 
+# run tests with -count=1 to avoid caching
+[doc]
 test-unit:
-    go test {{go_package_name}}/pkg/storage -run TestAllMethodsWithExistingStudent
+    go test -count=1 {{go_package_name}}/pkg/storage -run TestBasicUsage
--- pkg/storage/student_test.go
+++ pkg/storage/student_test.go
@@ -9,48 +9,50 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestAllMethodsWithExistingStudent(t *testing.T) {
-	dbCfg := storage.DBConfig{
-		Host:   "localhost",
-		Port:   "5432",
-		DBName: "school",
+func TestBasicUsage(t *testing.T) {
+	t.Run("Run all storage methods with a new student", func(t *testing.T) {
+		dbCfg := storage.DBConfig{
+			Host:   "localhost",
+			Port:   "5432",
+			DBName: "school",
 
-		Username: "postgres",
-		Password: "devpassword",
-	}
-	store, err := storage.NewStore(dbCfg)
-	require.NoError(t, err)
+			Username: "postgres",
+			Password: "devpassword",
+		}
+		store, err := storage.NewStore(dbCfg)
+		require.NoError(t, err)
 
-	// CreateStudent()
-	initialStudentName := "Alice"
-	createdStudent, err := store.CreateStudent(initialStudentName)
-	require.NoError(t, err)
-	require.NotEqualValues(t, uuid.Nil, createdStudent.ID)
-	require.EqualValues(t, "Alice", createdStudent.Name)
+		// CreateStudent()
+		initialStudentName := "Alice"
+		createdStudent, err := store.CreateStudent(initialStudentName)
+		require.NoError(t, err)
+		require.NotEqualValues(t, uuid.Nil, createdStudent.ID)
+		require.EqualValues(t, "Alice", createdStudent.Name)
 
-	// GetStudentByID()
-	fetchedStudent, found, err := store.GetStudentByID(createdStudent.ID)
-	require.NoError(t, err)
-	require.True(t, found)
-	require.EqualValues(t, "Alice", fetchedStudent.Name)
+		// GetStudentByID()
+		fetchedStudent, found, err := store.GetStudentByID(createdStudent.ID)
+		require.NoError(t, err)
+		require.True(t, found)
+		require.EqualValues(t, "Alice", fetchedStudent.Name)
 
-	// UpdateStudent()
-	studentWithNameChanged := &models.Student{
-		ID:   createdStudent.ID,
-		Name: "Bob",
-	}
-	updatedStudent, found, err := store.UpdateStudent(studentWithNameChanged)
-	require.NoError(t, err)
-	require.True(t, found)
-	require.EqualValues(t, "Bob", updatedStudent.Name)
+		// UpdateStudent()
+		studentWithNameChanged := &models.Student{
+			ID:   createdStudent.ID,
+			Name: "Bob",
+		}
+		updatedStudent, found, err := store.UpdateStudent(studentWithNameChanged)
+		require.NoError(t, err)
+		require.True(t, found)
+		require.EqualValues(t, "Bob", updatedStudent.Name)
 
-	// DeleteStudent()
-	found, err = store.DeleteStudentByID(updatedStudent.ID)
-	require.NoError(t, err)
-	require.True(t, found)
+		// DeleteStudent()
+		found, err = store.DeleteStudentByID(updatedStudent.ID)
+		require.NoError(t, err)
+		require.True(t, found)
 
-	// check that student was deleted
-	_, found, err = store.GetStudentByID(updatedStudent.ID)
-	require.NoError(t, err)
-	require.False(t, found)
+		// check that student was deleted
+		_, found, err = store.GetStudentByID(updatedStudent.ID)
+		require.NoError(t, err)
+		require.False(t, found)
+	})
 }
